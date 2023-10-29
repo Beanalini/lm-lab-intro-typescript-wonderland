e08eff467aa0bc1c47cd8949823443fbe9bf24c7
Beanalini <dat826@gmail.com>
2023-10-29 19:45:51 +0000

    feat: add custom Tea type

--- src/chapter_7/chapter_7_choose_tea.ts
+++ src/chapter_7/chapter_7_choose_tea.ts
@@ -1,6 +1,16 @@
 import { haveAdventures } from "../..";
 import { askQuestion, clear, print } from "../ui/console";
 
+const TEAS = [
+  "Darjeeling",
+  "Earl Grey",
+  "Camomile",
+  "Ginger",
+  "Tetley",
+] as const;
+
+type Tea = (typeof TEAS)[number];
+
 export function chooseTea() {
   print("Chapter 7");
 }
